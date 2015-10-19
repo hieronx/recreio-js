552a11b255bf7cca75b3227b9685a0da2b750089
Gert Spek <gert@42education.com>
2015-10-19 11:52:38 +0200

    Update typescript definitions

--- recreio.d.ts
+++ recreio.d.ts
@@ -2,24 +2,30 @@
 // Project: https://github.com/recreio/recreio-js
 // Definitions by: Jeroen Offerijns <https://github.com/offerijns>, Gert Spek <https://github.com/spekkie1994>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
+
 /// <reference path="typings/bluebird/bluebird.d.ts" />
 
-
 declare module RecreIO {
+
+    interface Curriculum {
+        option: String;
+    }
 
     interface Exercise {
         template: string;
         pattern?: string;
         instruction?: string;
-        curriculum?: string;
+        curriculum?: Curriculum[];
 
         begin(): Exercise;
         save(success: boolean): any;
     }
 
+    function signIn(provider: any): Promise<any>;
+    function signInWithUsername(username: string, password: string): Promise<any>;
+    function signInWithEmail(email: string, password: string): Promise<any>;
     function getAccount(): Promise<any>;
-    function signIn(provider: any): Promise<any>
-    function signInWithUsername(username: string, password: string): Promise<any>
-    function signInWithEmail(email: string, password: string): Promise<any>
+    
     function getNextExercise(template: string): Promise<Exercise>;
+    
 }
