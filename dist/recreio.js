d94adb766c316edb5478027964dd8e5ec43da819
Jeroen Offerijns <jeroen@offerijns.nl>
2016-04-28 14:36:20 +0200

    Fix missing get parameter by name method in client

--- dist/recreio.js
+++ dist/recreio.js
@@ -393,6 +393,11 @@ var RecreIO;
                     setTimeout(function () { document.getElementById('last-achievement').outerHTML = ''; }, 4000);
                 }
             };
+            this.getParameterByName = function (name) {
+                name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+                var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"), results = regex.exec(location.search);
+                return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+            };
             /**
              *
              */
--- src/main.ts
+++ src/main.ts
@@ -174,6 +174,12 @@ module RecreIO {
       }
     }
 
+    private getParameterByName = (name: string) => {
+      name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+      var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+          results = regex.exec(location.search);
+      return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
 
     /**
      *
