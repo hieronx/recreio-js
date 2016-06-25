80ff122b07dbb13727436d52b7b8bbbc942722ca
Jeroen Offerijns <jeroen@offerijns.nl>
2016-06-25 19:53:10 +0200

    Fix content type of leaderboard result requests

--- src/Leaderboard.ts
+++ src/Leaderboard.ts
@@ -11,7 +11,7 @@ module RecreIO {
     constructor(private id: number, private client: RecreIO.Client) {}
 
     public submitResult = (result: string): void => {
-      this.client.sendRequest('POST', 'leaderboards/' + this.id + '/results', {}, result).catch((exception: any) => {
+      this.client.sendRequest('POST', 'leaderboards/' + this.id + '/results', result, {}, 'text/plain').catch((exception: any) => {
         console.error('Oops, submitting the result did not work..');
       });
     }
